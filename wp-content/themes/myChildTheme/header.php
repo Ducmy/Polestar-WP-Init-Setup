42f7925fd466c2b67879ac5bdbe5ce2a70188b05
cnvs_my <mynguyen@Macpro-my.local>
2020-08-05 20:13:33 +0700

    Update plugin

--- wp-content/themes/myChildTheme/header.php
+++ wp-content/themes/myChildTheme/header.php
@@ -26,13 +26,14 @@
 		<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> 
 	-->
-	<?php if (is_single()) :
+	<?php
+	if (is_single('product')) {
 		$uniqueLoad = array(
 			array("type" => "css", "src" => get_stylesheet_directory_uri()   . "/includes/css/single.css"),
 			array("type" => "js", "src" => get_stylesheet_directory_uri() . "/includes/js/single.js"),
 		);
-	endif;
-	echo loadUniqueFile($uniqueLoad);
+		echo loadUniqueFile($uniqueLoad);
+	}
 	?>
 </head>
 
